e5882147766490828d46c9000541643ea50d14ec
Tunguso4ka <71643624+Tunguso4ka@users.noreply.github.com>
2021-03-25 21:26:28 +0300

    yeap, im dumb

--- ProgectMangoRF/Properties/Settings.Designer.cs
+++ ProgectMangoRF/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ProjectMangoRF.Properties {
+namespace ProgectMangoRF.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
