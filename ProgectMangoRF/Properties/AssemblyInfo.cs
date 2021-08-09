c83deed1755805bbfff0b95b45f663257f4cedb6
Tunguso4ka <71643624+Tunguso4ka@users.noreply.github.com>
2021-08-09 18:48:53 +0300

    0.9 Release

--- ProgectMangoRF/Properties/AssemblyInfo.cs
+++ ProgectMangoRF/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Windows;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ProjectMangoRF")]
+[assembly: AssemblyProduct("RandomFights")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
